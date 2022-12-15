aa7f3fd3614a215d2a5673971a8bcc89c329306b
Greg Cotten <gregcotten@users.noreply.github.com>
2022-12-15 00:07:22 -0600

    will it blend?

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -9,21 +9,27 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "Minizip",
-            dependencies: [],
-            path: "Zip/minizip",
-            exclude: ["module"],
-            linkerSettings: [
-                .linkedLibrary("z")
+            name: "zlib",
+            publicHeadersPath: "include",
+            cSettings: [
+                .unsafeFlags(["-Wno-shorten-64-to-32"]),
+                .define("Z_HAVE_UNISTD_H", .when(platforms: [.macOS, .linux])),
+                .define("HAVE_STDARG_H"),
+                .define("HAVE_HIDDEN"),
+                .define("_CRT_SECURE_NO_DEPRECATE", .when(platforms: [.windows])),
+                .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
             ]),
         .target(
+            name: "Minizip",
+            dependencies: ["zlib"]),
+        .target(
             name: "Zip",
-            dependencies: ["Minizip"],
-            path: "Zip",
-            exclude: ["minizip", "zlib"]),
+            dependencies: ["Minizip"]),
         .testTarget(
             name: "ZipTests",
             dependencies: ["Zip"],
-            path: "ZipTests"),
+            resources: [
+                .process("Resources"),
+            ]),
     ]
 )
